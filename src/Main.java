2371b3a97cded7ecff5adaad0be260ef9cf748c8
olegfili <oleg.fil_891@mail.ru>
2024-04-03 18:44:12 +0300

    дз Объекты и классы

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Author tolstoy = new Author(" Лев " , "Толстой");
+        Author dostoevsky = new Author(" Фёдор ", "Достаевский");
+        Book warAndPeace = new Book ("Война и Мир", tolstoy , 1867);
+        Book brothersKaramazov  = new Book ("Братья Карамазовы" , dostoevsky , 1880);
+        warAndPeace.setPublicationYear(1900);
+        System.out.println("Название книги: " + warAndPeace.getBookName()+ ". Автор: " + warAndPeace.getAuthor().getFirstName() + warAndPeace.getAuthor().getLastName() + ". Год издания: " + warAndPeace.getPublicationYear());
+        System.out.println("Название книги: "  + brothersKaramazov.getBookName() + ". Автор: " + brothersKaramazov.getAuthor().getFirstName() + brothersKaramazov.getAuthor().getLastName()+ ". Год издания: " + brothersKaramazov.getPublicationYear() );
+
+    }
+}
